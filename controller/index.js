9f033b5749247f08467e40c4ee15c911ac0e364d
circlefour <0100.circle@gmail.com>
2025-09-17 17:15:26 +0200

    added a red gradient in front end for shaking intensity feedback

--- controller/index.js
+++ controller/index.js
@@ -3,22 +3,30 @@ const socket = io();
 const btn = document.getElementById("btn");
 
 function setupDeviceMotionListener() {
+  let smoothInt = 0;
+  const alpha = 0.1; // smoothing factor (0-1), smaller = smoother
+
   window.addEventListener("devicemotion", (event) => {
     //console.log('hi wussup');
 
     const acceleration = event.accelerationIncludingGravity;
 
-    // i used to use this, but now i don't really, but i'll keep it for the ui to display how the value is changing, though the visual for changing values should be more intuitive
     const level = Math.sqrt(
       acceleration.x ** 2 + acceleration.y ** 2 + acceleration.z ** 2
     );
 
-    //console.log("shake level: ", level);
-    document.getElementById("num").textContent = `${Math.round(level)}`;
-
-    // prior thing
-    //socket.emit('shake', level);
     socket.emit('shake', {x: acceleration.x, y: acceleration.y, z: acceleration.z, avg: level});
+
+    const minAccel = 9.81;
+    const maxAccel = 55;
+
+    let intensity = (level - minAccel) / (maxAccel - minAccel);
+    intensity = Math.max(0, Math.min(1, intensity));
+
+    // smooth intensity
+    smoothInt = smoothInt * (1 - alpha) + intensity * alpha;
+    const r = Math.round(255 * smoothInt);
+    document.body.style.backgroundColor = `rgb(${r},0,0)`;
   });
 }
 
